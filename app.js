156d0b75f35746acc7ebc78df4a533a6a183c7ea
Noel Khasipov <noel@noelkhasipov.com>
2017-02-22 16:04:57 +0700

    spaces and commas

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ fetch('font/04b03.json').then(function(response) {
   var img = new Image();
   img.src = 'font/04b03.png';
 
-  var str = "_Hello, World!";
+  var str = "Hello, World and Me!";
 
   var buf = [];
       
   for ( var i = str.length - 1; i >= 0; i-- ) {
-    buf.unshift(str[i].charCodeAt().toString());
+    var charCode = str[i].charCodeAt().toString();
+    buf.unshift(charCode);
   }
 
   var offsetX = 0;
@@ -22,12 +23,19 @@ fetch('font/04b03.json').then(function(response) {
   for (var i = 0; i < buf.length; i++) {
     var charIndex = font.chars.char.findIndex(function(obj) { return obj["_id"] === buf[i]; });
     var char = font.chars.char[charIndex];
-    console.log(char);
 
-    offsetY = 5 - char._height;
+    if ( buf[i] === "44" ) {
+      offsetY = 6 - char._height;
+    } else {
+      offsetY = 5 - char._height;
+    }
 
     ctx.drawImage(img, char._x, char._y, char._width, char._height, offsetX, offsetY, char._width, char._height);
 
-    offsetX += parseInt(char._width) + 1; 
+    if ( buf[i] === "32" ) {
+      offsetX += parseInt(char._width) + 3;
+    } else {
+      offsetX += parseInt(char._width) + 1;
+    }
   }
 });
