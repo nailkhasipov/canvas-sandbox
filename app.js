b8e0bc0ff3e40c02a969c6a4eb37ccd74e907c17
Noel Khasipov <noel@noelkhasipov.com>
2017-02-23 12:36:11 +0700

    player jump draft

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 
+var groundY = 40;
+var gravity = 0.2;
+var speed = 2;
+
+player = { x: 10, y: groundY, jumping: false, velY: 0, img: 'player.png' }
+var playerImage = new Image();
+playerImage.src = player.img;
+
 fetch('font/04b03.json').then(function(response) { 
   return response.json();
 }).then(function(data) {
@@ -9,7 +17,7 @@ fetch('font/04b03.json').then(function(response) {
   var img = new Image();
   img.src = 'font/04b03.png';
 
-  var str = "Hello, World and Me!";
+  var str = "Hello, World!";
 
   var buf = [];
       
@@ -39,3 +47,41 @@ fetch('font/04b03.json').then(function(response) {
     }
   }
 });
+
+function drawPlayer() {
+  ctx.drawImage(playerImage, 10, player.y);
+}
+
+function drawGround() {
+  ctx.beginPath();
+  ctx.moveTo(0,52.5);
+  ctx.lineTo(128,52.5);
+  ctx.lineWidth = 1;
+  ctx.stroke();
+}
+
+document.addEventListener("keydown", function(e) {
+  if(e.keyCode == 88 && player.jumping == false) {
+    player.jumping = true;
+    player.velY = -speed*2;
+  }
+});
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  player.velY += gravity;
+  player.y += player.velY;
+
+  if(player.y >= groundY){
+    player.y = groundY;
+    player.jumping = false;
+  }
+
+  drawGround();
+  drawPlayer();
+
+  requestAnimationFrame(draw);
+}
+
+draw();
